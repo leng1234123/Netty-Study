6a82d2e8da502cb15e257959f86be683736d8be6
leng_glb <leng_glb@qq.com>
2018-12-26 17:47:58 +0800

    echo

--- src/main/java/com/netty/echo/server/EchoServer.java
+++ src/main/java/com/netty/echo/server/EchoServer.java
@@ -23,14 +23,17 @@ public class EchoServer {
 	}
     
     public void start() throws InterruptedException{
-    	EventLoopGroup bossGroup = new NioEventLoopGroup(1); //负责处理客户端连接一个线程
-    	EventLoopGroup workerGroup = new NioEventLoopGroup();//读写线程默认处理器数量 * 2
+    	EventLoopGroup bossGroup = new NioEventLoopGroup(1); //负责处理客户端连接线程
+    	EventLoopGroup workerGroup = new NioEventLoopGroup();//读写线程默认CPU处理器数量 * 2
     	try {
-			ServerBootstrap bootstrap = new ServerBootstrap();//创建 ServerBootstrap
-			bootstrap.group(bossGroup,workerGroup)
-			.channel(NioServerSocketChannel.class) //指定使用 NIO的传输Channel，调用bind方法时会进行初始化
+			ServerBootstrap bootstrap = new ServerBootstrap();//创建 ServerBootstrap 辅助类
+			bootstrap
+			.group(bossGroup) //单线程处理 --> Reactor单线程模型
+			.group(bossGroup, workerGroup)//多线程处理 --> Reactor多线程模型
+			.channel(NioServerSocketChannel.class) //extends Channel，指定使用 NIO的传输Channel，调用bind方法时会进行初始化
 			.localAddress(port) //设置 socket 地址使用所选的端口
-			.childHandler(new ChannelInitializer<SocketChannel>() { //添加 EchoServerHandler 到 Channel 的 ChannelPipeline
+			//extends ChannelHandler，添 handlers到 Channel中的的ChannelPipeline
+			.childHandler(new ChannelInitializer<SocketChannel>() { 
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
 					ch.pipeline().addLast(
@@ -43,7 +46,7 @@ public class EchoServer {
     	} catch (Exception e) {
 			e.printStackTrace();
 		} finally{
-			bossGroup.shutdownGracefully().sync();//关闭 EventLoopGroup，释放所有资源。
+			bossGroup.shutdownGracefully().sync();//关闭 EventLoopGroup，释放所有资源
 			workerGroup.shutdownGracefully().sync();
 		}
     }
