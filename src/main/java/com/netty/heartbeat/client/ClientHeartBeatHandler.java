0d33469e60c14a714cdf44ceaf2d435ab02034b3
lengyul <1174959274@qq.com>
2019-01-17 16:41:46 +0800

    HeartBeat: clienthandler

--- src/main/java/com/netty/heartbeat/client/ClientHeartBeatHandler.java
+++ src/main/java/com/netty/heartbeat/client/ClientHeartBeatHandler.java
@@ -30,11 +30,19 @@ public class ClientHeartBeatHandler extends ChannelInboundHandlerAdapter {
                      如果channelHandler有同样的方法时,只会调用一次,
                      如果需要后续的channelHandler调用需要调用fire开头的方法
         */
-        for (int i = 1; i <= 1000; i++) {
+       /* for (int i = 1; i <= 1000; i++) {
         	 String data = new String("The"+i+"number");
              msg = new HeartbeatMessage((byte)0xA2, data);
              ctx.channel().writeAndFlush(msg);
+		}*/
+        /*String str = "1234567890-=qwertyuiop[]asdfghjkl;zxcvbnm";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 300; i++) {			
+        	sb.append(str);
 		}
+        msg = new HeartbeatMessage((byte)0xA2, sb.toString() + "END");
+        ctx.channel().writeAndFlush(msg);*/
+        
     }
 	
 	/**
